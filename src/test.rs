6eb153e2171b91b3e374d7fdac3d664781e2d444
kajacx <kajacx@gmail.com>
2023-06-20 12:23:26 +0200

    Make cargo test more cross platform

--- src/test.rs
+++ src/test.rs
@@ -2,18 +2,29 @@ use std::process::Command;
 
 #[test]
 fn run_test_script() {
-    let x = Command::new("C:/Programs/Git/bin/bash.exe")
+    let output = Command::new(get_bash_executable())
         .current_dir("tests")
         .arg("-c")
         .arg("./run_all_tests.sh")
         .output()
         .unwrap();
 
-    eprintln!("Status: {}", x.status);
-    eprintln!("Std out: {}", String::from_utf8_lossy(&x.stdout));
-    eprintln!("Std err: {}", String::from_utf8_lossy(&x.stderr));
+    if !output.status.success() {
+        eprintln!("Status: {}", output.status);
+        eprintln!("Std out: {}", String::from_utf8_lossy(&output.stdout));
+        eprintln!("Std err: {}", String::from_utf8_lossy(&output.stderr));
 
-    if !x.status.success() {
         panic!("Test script did not exit successfully");
+    }
+}
+
+fn get_bash_executable() -> String {
+    if cfg!(windows) {
+        let output = Command::new("where").arg("bash").output().unwrap();
+        let output = String::from_utf8_lossy(&output.stdout);
+        let first = output.lines().next().unwrap();
+        first.into()
+    } else {
+        "bash".into()
     }
 }
