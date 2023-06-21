c21f4966ebc2c8ea6b872a6973a5771515b52771
kajacx <kajacx@gmail.com>
2023-06-21 13:05:11 +0200

    Add test with multiple arguments

--- tests/no_bindgen/call_exported_function_many_args/host.rs
+++ tests/no_bindgen/call_exported_function_many_args/host.rs
@@ -30,5 +30,20 @@ pub fn run_test(bytes: &[u8]) -> Result<(), Box<dyn Error>> {
     assert_eq!(a, 80i64 + 10);
     assert_eq!(b, 80i64 - 10);
 
+    // Many different arguments
+    let add_all =
+        instance.get_typed_func::<(i32, i64, u32, u64, f32, f64), f64>(&mut store, "add_all")?;
+
+    let result = add_all.call(&mut store, (10, 20, 30, 40, 50.25, 60.5))?;
+    assert_eq!(result, 10.0 + 20.0 + 30.0 + 40.0 + 50.25 + 60.5);
+
+    // Many arguments and results
+    // TODO: Compilation to WASM doesn't work, error in on Rust's end.
+    // let add_ten_i32s = instance
+    //     .get_typed_func::<(i32, i32, i32, i32), (i32, i32, i32, i32)>(&mut store, "add_ten_i32s")?;
+
+    // let result = add_ten_i32s.call(&mut store, (10, 20, 30, 40))?;
+    // assert_eq!(result, (20, 30, 40, 50));
+
     Ok(())
 }
--- tests/no_bindgen/call_exported_function_many_args/plugin.rs
+++ tests/no_bindgen/call_exported_function_many_args/plugin.rs
@@ -12,3 +12,19 @@ pub fn add_sub_ten_i32(number: i32) -> (i32, i32) {
 pub fn add_sub_ten_i64(number: i64) -> (i64, i64) {
     (number.wrapping_add(10), number.wrapping_sub(10))
 }
+
+#[no_mangle]
+pub fn add_all(a: i32, b: i64, c: u32, d: u64, e: f32, g: f64) -> f64 {
+    a as f64 + b as f64 + c as f64 + d as f64 + e as f64 + g
+}
+
+// TODO: Rust cannot compile code returning more than 2 values?
+// #[no_mangle]
+// pub fn add_ten_i32s(a: i32, b: i32, c: i32) -> (i32, i32, i32, i32) {
+//     (
+//         a.wrapping_add(10),
+//         b.wrapping_add(10),
+//         c.wrapping_add(10),
+//         d.wrapping_add(10),
+//     )
+// }
