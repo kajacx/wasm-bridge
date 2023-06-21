527b8ae0931853bbff820b0b207f76b85078aefb
kajacx <kajacx@gmail.com>
2023-06-21 11:12:20 +0200

    Implement FromJsResults for tuples

--- src/js/conversions/from_js_results.rs
+++ src/js/conversions/from_js_results.rs
@@ -4,18 +4,13 @@ use wasm_bindgen::JsValue;
 use crate::*;
 
 pub trait FromJsResults: Sized {
+    // fn number_of_results() -> u32; TODO: check return length?
     fn from_js_results(results: &JsValue) -> Result<Self, Error>;
 }
 
-impl<T: FromJsValue, U: FromJsValue> FromJsResults for (T, U) {
-    fn from_js_results(results: &JsValue) -> Result<Self, Error> {
-        let first = Reflect::get_u32(results, 0)?;
-        let first = T::from_js_value(&first)?;
-
-        let second = Reflect::get_u32(results, 1)?;
-        let second = U::from_js_value(&second)?;
-
-        Ok((first, second))
+impl FromJsResults for () {
+    fn from_js_results(_results: &JsValue) -> Result<Self, Error> {
+        Ok(())
     }
 }
 
@@ -24,6 +19,12 @@ macro_rules! from_js_results_single {
         impl FromJsResults for $ty {
             fn from_js_results(results: &JsValue) -> Result<Self, Error> {
                 <Self as FromJsValue>::from_js_value(results)
+            }
+        }
+
+        impl FromJsResults for ($ty,) {
+            fn from_js_results(results: &JsValue) -> Result<Self, Error> {
+                Ok((<$ty as FromJsValue>::from_js_value(results)?,))
             }
         }
     };
@@ -35,3 +36,36 @@ from_js_results_single!(u32);
 from_js_results_single!(u64);
 from_js_results_single!(f32);
 from_js_results_single!(f64);
+
+macro_rules! from_js_results_many {
+    ($(($index: tt, $name: ident)),*) => {
+        impl<$($name: FromJsValue),*> FromJsResults for ($($name, )*) {
+            fn from_js_results(results: &JsValue) -> Result<Self, Error> {
+                Ok(( $($name::from_js_value(&Reflect::get_u32(results, $index)?)?,)* ))
+            }
+        }
+    };
+}
+
+#[rustfmt::skip]
+from_js_results_many!((0, T0), (1, T1));
+#[rustfmt::skip]
+from_js_results_many!((0, T0), (1, T1), (2, T2));
+#[rustfmt::skip]
+from_js_results_many!((0, T0), (1, T1), (2, T2), (3, T3));
+#[rustfmt::skip]
+from_js_results_many!((0, T0), (1, T1), (2, T2), (3, T3), (4, T4));
+#[rustfmt::skip]
+from_js_results_many!((0, T0), (1, T1), (2, T2), (3, T3), (4, T4), (5, T5));
+#[rustfmt::skip]
+from_js_results_many!((0, T0), (1, T1), (2, T2), (3, T3), (4, T4), (5, T5), (6, T6));
+#[rustfmt::skip]
+from_js_results_many!((0, T0), (1, T1), (2, T2), (3, T3), (4, T4), (5, T5), (6, T6), (7, T7));
+#[rustfmt::skip]
+from_js_results_many!((0, T0), (1, T1), (2, T2), (3, T3), (4, T4), (5, T5), (6, T6), (7, T7), (8, T8));
+#[rustfmt::skip]
+from_js_results_many!((0, T0), (1, T1), (2, T2), (3, T3), (4, T4), (5, T5), (6, T6), (7, T7), (8, T8), (9, T9));
+#[rustfmt::skip]
+from_js_results_many!((0, T0), (1, T1), (2, T2), (3, T3), (4, T4), (5, T5), (6, T6), (7, T7), (8, T8), (9, T9), (10, T10));
+#[rustfmt::skip]
+from_js_results_many!((0, T0), (1, T1), (2, T2), (3, T3), (4, T4), (5, T5), (6, T6), (7, T7), (8, T8), (9, T9), (10, T10), (11, T11));
