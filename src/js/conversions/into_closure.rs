95cc7f29daf91675025b7acf0f70b80086e5f936
kajacx <kajacx@gmail.com>
2023-06-24 10:26:47 +0200

    Implement IntoClosure with a macro

--- src/js/conversions/into_closure.rs
+++ src/js/conversions/into_closure.rs
@@ -1,10 +1,4 @@
-use js_sys::Array;
-use wasm_bindgen::{
-    convert::{FromWasmAbi, IntoWasmAbi, ReturnWasmAbi, WasmAbi},
-    describe::WasmDescribe,
-    prelude::Closure,
-    JsValue,
-};
+use wasm_bindgen::{convert::FromWasmAbi, prelude::Closure, JsValue};
 
 use crate::*;
 
@@ -40,21 +34,69 @@ impl_into_closure_single!(u64);
 impl_into_closure_single!(f32);
 impl_into_closure_single!(f64);
 
-impl<P0, P1, R, F> IntoClosure<(P0, P1), R> for F
-where
-    F: Fn(Caller<()>, P0, P1) -> R + 'static,
-    P0: FromWasmAbi + 'static,
-    P1: FromWasmAbi + 'static,
-    R: IntoImportResults + 'static,
-{
-    fn into_closure(self) -> (JsValue, DropHandler) {
-        let closure =
-            Closure::<dyn Fn(P0, P1) -> R::Results + 'static>::new(move |arg0: P0, arg1: P1| {
-                self(Caller::new(), arg0, arg1).into_import_results()
-            });
+// impl<P0, P1, R, F> IntoClosure<(P0, P1), R> for F
+// where
+//     F: Fn(Caller<()>, P0, P1) -> R + 'static,
+//     P0: FromWasmAbi + 'static,
+//     P1: FromWasmAbi + 'static,
+//     R: IntoImportResults + 'static,
+// {
+//     fn into_closure(self) -> (JsValue, DropHandler) {
+//         let closure =
+//             Closure::<dyn Fn(P0, P1) -> R::Results + 'static>::new(move |arg0: P0, arg1: P1| {
+//                 self(Caller::new(), arg0, arg1).into_import_results()
+//             });
 
-        let js_val: JsValue = closure.as_ref().into();
+//         let js_val: JsValue = closure.as_ref().into();
 
-        (js_val, DropHandler::new(closure))
-    }
+//         (js_val, DropHandler::new(closure))
+//     }
+// }
+
+macro_rules! into_closure_many {
+    ($(($param: ident, $name: ident)),*) => {
+        impl<$($name, )* R, F> IntoClosure<($($name),*), R> for F
+        where
+            F: Fn(Caller<()> $(, $name)*) -> R + 'static,
+            $($name: FromWasmAbi + 'static,)*
+            R: IntoImportResults + 'static,
+        {
+            fn into_closure(self) -> (JsValue, DropHandler) {
+                let closure =
+                    Closure::<dyn Fn($($name),*) -> R::Results + 'static>::new(move |$($param: $name),*| {
+                        self(Caller::new() $(, $param)*).into_import_results()
+                    });
+
+                let js_val: JsValue = closure.as_ref().into();
+
+                (js_val, DropHandler::new(closure))
+            }
+        }
+    };
 }
+
+#[rustfmt::skip]
+into_closure_many!((p0, P0), (p1, P1));
+#[rustfmt::skip]
+into_closure_many!((p0, P0), (p1, P1), (p2, P2));
+#[rustfmt::skip]
+into_closure_many!((p0, P0), (p1, P1), (p2, P2), (p3, P3));
+#[rustfmt::skip]
+into_closure_many!((p0, P0), (p1, P1), (p2, P2), (p3, P3), (p4, P4));
+#[rustfmt::skip]
+into_closure_many!((p0, P0), (p1, P1), (p2, P2), (p3, P3), (p4, P4), (p5, P5));
+#[rustfmt::skip]
+into_closure_many!((p0, P0), (p1, P1), (p2, P2), (p3, P3), (p4, P4), (p5, P5), (p6, P6));
+#[rustfmt::skip]
+into_closure_many!((p0, P0), (p1, P1), (p2, P2), (p3, P3), (p4, P4), (p5, P5), (p6, P6), (p7, P7));
+
+// js-sys doesn't support closured with more than 7 arguments
+
+// #[rustfmt::skip]
+// into_closure_many!((p0, P0), (p1, P1), (p2, P2), (p3, P3), (p4, P4), (p5, P5), (p6, P6), (p7, P7), (p8, P8));
+// #[rustfmt::skip]
+// into_closure_many!((p0, P0), (p1, P1), (p2, P2), (p3, P3), (p4, P4), (p5, P5), (p6, P6), (p7, P7), (p8, P8), (p9, P9));
+// #[rustfmt::skip]
+// into_closure_many!((p0, P0), (p1, P1), (p2, P2), (p3, P3), (p4, P4), (p5, P5), (p6, P6), (p7, P7), (p8, P8), (p9, P9), (p10, P10));
+// #[rustfmt::skip]
+// into_closure_many!((p0, P0), (p1, P1), (p2, P2), (p3, P3), (p4, P4), (p5, P5), (p6, P6), (p7, P7), (p8, P8), (p9, P9), (p10, P10), (p11, P11));
