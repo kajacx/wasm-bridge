d7d76fb050b32207d93caad125a595bb4adb0743
kajacx <kajacx@gmail.com>
2023-06-24 17:07:18 +0200

    Remove no-bindgen-wat tests
    Wat tests will be in normal no-bindgen tests instead

--- tests/skeletons/no_bindgen_wat/host_js/src/main.rs
+++
@@ -1,8 +0,0 @@
-const PLUGIN_WAT: &'static [u8] = include_bytes!("../../plugin/plugin.wat");
-
-mod host;
-
-#[wasm_bindgen_test::wasm_bindgen_test]
-fn main() {
-    host::run_test(PLUGIN_WAT).expect("host_js test should pass")
-}
--- tests/skeletons/no_bindgen_wat/host_sys/src/main.rs
+++
@@ -1,7 +0,0 @@
-const PLUGIN_WAT: &'static [u8] = include_bytes!("../../plugin/plugin.wat");
-
-mod host;
-
-fn main() {
-    host::run_test(PLUGIN_WAT).expect("host_sys test should pass")
-}
