c8342b732d975392044df1d1b8aa6396b79d295c
kajacx <kajacx@gmail.com>
2023-06-24 17:14:10 +0200

    Merge exported fns tests

--- tests/no_bindgen/call_exported_function/host.rs
+++ tests/no_bindgen/call_exported_function/host.rs
@@ -3,59 +3,81 @@ use wasm_bridge::*;
 
 pub fn run_test(bytes: &[u8]) -> Result<(), Box<dyn Error>> {
     let mut store = Store::<()>::default();
-
     let module = Module::new(&store.engine(), bytes)?;
-
     let instance = Instance::new(&mut store, &module, &[])?;
 
+    single_value(&mut store, &instance)?;
+    few_values(&mut store, &instance)?;
+
+    Ok(())
+}
+
+fn single_value(store: &mut Store<()>, instance: &Instance) -> Result<(), Box<dyn Error>> {
     // Signed integers
-    let add_five_i32 = instance.get_typed_func::<i32, i32>(&mut store, "add_five_i32")?;
+    let add_five_i32 = instance.get_typed_func::<i32, i32>(store, "add_five_i32")?;
 
     for number in [-10, -1, 0, 10, i32::MIN + 1, i32::MAX - 2] {
-        let returned = add_five_i32.call(&mut store, number)?;
+        let returned = add_five_i32.call(store, number)?;
         assert_eq!(returned, number.wrapping_add(5));
     }
 
-    let add_five_i64 = instance.get_typed_func::<i64, i64>(&mut store, "add_five_i64")?;
+    let add_five_i64 = instance.get_typed_func::<i64, i64>(store, "add_five_i64")?;
 
     for number in [-10, -1, 0, 10, i64::MIN + 1, i64::MAX - 2] {
-        let returned = add_five_i64.call(&mut store, number)?;
+        let returned = add_five_i64.call(store, number)?;
         assert_eq!(returned, number.wrapping_add(5));
     }
 
     // Unsigned integers
-    let add_five_u32 = instance.get_typed_func::<u32, u32>(&mut store, "add_five_i32")?;
+    let add_five_u32 = instance.get_typed_func::<u32, u32>(store, "add_five_i32")?;
 
     for number in [0, 10, u32::MAX / 2 - 1, u32::MAX - 2] {
-        let returned = add_five_u32.call(&mut store, number)?;
+        let returned = add_five_u32.call(store, number)?;
         assert_eq!(returned, number.wrapping_add(5));
     }
 
-    let add_five_u64 = instance.get_typed_func::<u64, u64>(&mut store, "add_five_i64")?;
+    let add_five_u64 = instance.get_typed_func::<u64, u64>(store, "add_five_i64")?;
 
     for number in [0, 10, u64::MAX / 2 - 1, u64::MAX - 2] {
-        let returned = add_five_u64.call(&mut store, number)?;
+        let returned = add_five_u64.call(store, number)?;
         assert_eq!(returned, number.wrapping_add(5));
     }
 
     // Floats
-    let add_five_f32 = instance.get_typed_func::<f32, f32>(&mut store, "add_five_f32")?;
+    let add_five_f32 = instance.get_typed_func::<f32, f32>(store, "add_five_f32")?;
 
     for number in [0.0, 10.25, -2.5, 1_000_000.5, -1_000_000.5] {
-        let returned = add_five_f32.call(&mut store, number)?;
+        let returned = add_five_f32.call(store, number)?;
         assert_eq!(returned, number + 5.0);
     }
 
-    let add_five_f64 = instance.get_typed_func::<f64, f64>(&mut store, "add_five_f64")?;
+    let add_five_f64 = instance.get_typed_func::<f64, f64>(store, "add_five_f64")?;
 
     for number in [0.0, 10.25, -2.5, 10_000_000_000.5, -10_000_000_000.5] {
-        let returned = add_five_f64.call(&mut store, number)?;
+        let returned = add_five_f64.call(store, number)?;
         assert_eq!(returned, number + 5.0);
     }
 
+    Ok(())
+}
+
+fn few_values(store: &mut Store<()>, instance: &Instance) -> Result<(), Box<dyn Error>> {
+    // Multiple arguments
+    let add_i32 = instance.get_typed_func::<(i32, i32), i32>(store, "add_i32")?;
+
+    let returned = add_i32.call(store, (5, 10))?;
+    assert_eq!(returned, 5 + 10);
+
+    // Multiple results
+    let add_sub_ten_i32 = instance.get_typed_func::<i32, (i32, i32)>(store, "add_sub_ten_i32")?;
+
+    let (a, b) = add_sub_ten_i32.call(store, 50)?;
+    assert_eq!(a, 50i32 + 10);
+    assert_eq!(b, 50i32 - 10);
+
     // Single-element tuple
-    let add_five_f64 = instance.get_typed_func::<(f64,), (f64,)>(&mut store, "add_five_f64")?;
-    let returned = add_five_f64.call(&mut store, (5.5,))?;
+    let add_five_f64 = instance.get_typed_func::<(f64,), (f64,)>(store, "add_five_f64")?;
+    let returned = add_five_f64.call(store, (5.5,))?;
     assert_eq!(returned, (5.5 + 5.0,));
 
     Ok(())
--- tests/no_bindgen/call_exported_function/plugin.rs
+++ tests/no_bindgen/call_exported_function/plugin.rs
@@ -1,3 +1,5 @@
+// Single value
+
 #[no_mangle]
 pub fn add_five_i32(number: i32) -> i32 {
     number.wrapping_add(5)
@@ -16,4 +18,18 @@ pub fn add_five_f32(number: f32) -> f32 {
 #[no_mangle]
 pub fn add_five_f64(number: f64) -> f64 {
     number + 5.0
+}
+
+// Multiple params
+
+#[no_mangle]
+pub fn add_i32(a: i32, b: i32) -> i32 {
+    a.wrapping_add(b)
+}
+
+// Multiple results
+
+#[no_mangle]
+pub fn add_sub_ten_i32(number: i32) -> (i32, i32) {
+    (number.wrapping_add(10), number.wrapping_sub(10))
 }
