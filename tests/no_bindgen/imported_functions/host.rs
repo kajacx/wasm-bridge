1b7890b0991e6296cc8e4cf6036be5779ef1fe33
kajacx <kajacx@gmail.com>
2023-06-25 20:19:39 +0200

    Unify imported fs tests

--- tests/no_bindgen/imported_functions/host.rs
+++ tests/no_bindgen/imported_functions/host.rs
@@ -7,6 +7,8 @@ pub fn run_test(bytes: &[u8]) -> Result<(), Box<dyn Error>> {
     let module = Module::new(&store.engine(), bytes)?;
 
     let mut linker = Linker::new(store.engine());
+
+    // Single value
     linker.func_wrap("imported_fns", "add_one_i32", |_: Caller<()>, val: i32| {
         val.wrapping_add(1)
     })?;
@@ -19,8 +21,33 @@ pub fn run_test(bytes: &[u8]) -> Result<(), Box<dyn Error>> {
     linker.func_wrap("imported_fns", "add_one_f64", |_: Caller<()>, val: f64| {
         val + 1.0
     })?;
+
+    // Multiple values
+    linker.func_wrap(
+        "imported_fns",
+        "add_i32_import",
+        |_: Caller<()>, a: i32, b: i32| a.wrapping_add(b),
+    )?;
+    linker.func_wrap(
+        "imported_fns",
+        "add_sub_ten_import",
+        |_: Caller<()>, num: i32| (num.wrapping_add(10), num.wrapping_sub(10)),
+    )?;
+
+    // No arguments (interion mutability), ... TODO
+    // let global_value = Rc::new(Cell::new(5i32));
+    // let global_clone = global_value.clone();
+
     let instance = linker.instantiate(&mut store, &module)?;
 
+    single_value(&mut store, &instance)?;
+    few_values(&mut store, &instance)?;
+    many_values(&mut store)?;
+
+    Ok(())
+}
+
+fn single_value(mut store: &mut Store<()>, instance: &Instance) -> Result<(), Box<dyn Error>> {
     // Signed integers
     let add_three_i32 = instance.get_typed_func::<i32, i32>(&mut store, "add_three_i32")?;
 
@@ -65,6 +92,62 @@ pub fn run_test(bytes: &[u8]) -> Result<(), Box<dyn Error>> {
         let returned = add_three_f64.call(&mut store, number)?;
         assert_eq!(returned, number + 3.0);
     }
+
+    Ok(())
+}
+
+fn few_values(mut store: &mut Store<()>, instance: &Instance) -> Result<(), Box<dyn Error>> {
+    // Two arguments
+    let add_i32 = instance.get_typed_func::<(i32, i32), i32>(&mut store, "add_i32")?;
+    let returned = add_i32.call(&mut store, (5, 15))?;
+    assert_eq!(returned, 20);
+
+    // Two return values
+    let add_sub_ten = instance.get_typed_func::<i32, (i32, i32)>(&mut store, "add_sub_ten")?;
+    let returned = add_sub_ten.call(&mut store, 20)?;
+    assert_eq!(returned, (30, 10));
+
+    Ok(())
+}
+
+fn many_values(mut store: &mut Store<()>) -> Result<(), Box<dyn Error>> {
+    let wat = r#"(module
+        (type $t0 (func (param i32 i64 i32 i64 f32 f64) (result i32 i64 i32 i64 f32 f64)))
+        (import "imported_fns" "add_import" (func $add_import (type $t0)))
+        (func $add (export "add") (type $t0)
+          (param $p0 i32) (param $p1 i64) (param $p2 i32) (param $p3 i64) (param $p4 f32) (param $p5 f64)
+          (result i32 i64 i32 i64 f32 f64)
+            (call $add_import
+              (local.get $p0)
+              (local.get $p1)
+              (local.get $p2)
+              (local.get $p3)
+              (local.get $p4)
+              (local.get $p5)
+            )
+        )
+        (table $T0 1 1 funcref)
+    )
+    "#;
+
+    let module = Module::new(&store.engine(), wat.as_bytes())?;
+
+    let mut linker = Linker::new(store.engine());
+    linker.func_wrap(
+        "imported_fns",
+        "add_import",
+        |_: Caller<()>, a: i32, b: i64, c: u32, d: u64, e: f32, f: f64| {
+            (a + 1, b + 1, c + 1, d + 1, e + 1.0, f + 1.0)
+        },
+    )?;
+    let instance = linker.instantiate(&mut store, &module)?;
+
+    let add = instance
+        .get_typed_func::<(i32, i64, u32, u64, f32, f64), (i32, i64, u32, u64, f32, f64)>(
+            &mut store, "add",
+        )?;
+    let returned = add.call(&mut store, (5, 15, 25, 35, 45.5, 55.5))?;
+    assert_eq!(returned, (6, 16, 26, 36, 46.5, 56.5));
 
     Ok(())
 }
--- tests/no_bindgen/imported_functions/plugin.rs
+++ tests/no_bindgen/imported_functions/plugin.rs
@@ -1,3 +1,4 @@
+// Single value
 #[no_mangle]
 pub fn add_three_i32(number: i32) -> i32 {
     unsafe { add_one_i32(number.wrapping_add(1)).wrapping_add(1) }
@@ -18,10 +19,45 @@ pub fn add_three_f64(number: f64) -> f64 {
     unsafe { add_one_f64(number + 1.0) + 1.0 }
 }
 
+// Multiple params
+#[no_mangle]
+pub fn add_i32(a: i32, b: i32) -> i32 {
+    unsafe { add_i32_import(a, b) }
+}
+
+// Multiple results
+#[no_mangle]
+pub fn add_sub_ten(num: i32) -> (i32, i32) {
+    unsafe { add_sub_ten_import(num) }
+}
+
+// No arguments
+// pub fn increment_twice() {
+//     unsafe {
+//         increment();
+//         increment();
+//     }
+// }
+
+// // Panic in imported fn
+// #[no_mangle]
+// pub fn panics() {
+//     unsafe {
+//         panics_import();
+//     }
+// }
+
 #[link(wasm_import_module = "imported_fns")]
 extern "C" {
     fn add_one_i32(number: i32) -> i32;
     fn add_one_i64(number: i64) -> i64;
     fn add_one_f32(number: f32) -> f32;
     fn add_one_f64(number: f64) -> f64;
+
+    fn add_i32_import(a: i32, b: i32) -> i32;
+    #[allow(improper_ctypes)]
+    fn add_sub_ten_import(num: i32) -> (i32, i32);
+
+    // fn increment();
+    // fn panics_import();
 }
