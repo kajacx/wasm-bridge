4fec1f154e4d91dd4ef5c65957625df06b2e08df
kajacx <kajacx@gmail.com>
2023-07-02 20:16:41 +0200

    Add js host wit test

--- tests/skeletons/wit_components/host_js/src/main.rs
+++ tests/skeletons/wit_components/host_js/src/main.rs
@@ -1,9 +1,8 @@
-const PLUGIN_BYTES: &'static [u8] =
-    include_bytes!("../../plugin/target/wasm32-unknown-unknown/debug/no_bindgen_plugin.wasm");
+const PLUGIN_PATH: &'static str = "../../plugin/target/wasm32-unknown-unknown/debug/out-dir/";
 
 mod host;
 
 #[wasm_bindgen_test::wasm_bindgen_test]
 fn main() {
-    host::run_test(PLUGIN_BYTES).expect("host_js test should pass")
+    host::run_test(PLUGIN_PATH.as_bytes()).expect("host_js test should pass")
 }
