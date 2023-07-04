ccc078849d0c6dbf45e5669af1cf80c075e0fe1e
kajacx <kajacx@gmail.com>
2023-07-04 20:27:11 +0200

    Do not check sys warnings to speed up action

--- run_checks.sh
+++ run_checks.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/sh
 set -e
 
-RUSTFLAGS="-D warnings" cargo check --all-features
+# RUSTFLAGS="-D warnings" cargo check --all-features
 RUSTFLAGS="-D warnings" cargo check --all-features --target=wasm32-unknown-unknown
 
-cargo clippy --all-features -- -D clippy::all
-cargo clippy --all-features --target=wasm32-unknown-unknown -- -D clippy::all
+# RUSTFLAGS="-D warnings" cargo clippy --all-features -- -D clippy::all
+RUSTFLAGS="-D warnings" cargo clippy --all-features --target=wasm32-unknown-unknown -- -D clippy::all
 
 cd tests
 ./run_all_tests.sh
