b956aa3395b84f426cdcf626fd8bb81781f77f08
kajacx <kajacx@gmail.com>
2023-07-05 20:17:36 +0200

    Add format check

--- run_checks.sh
+++ run_checks.sh
@@ -7,5 +7,7 @@ RUSTFLAGS="-D warnings" cargo check --all-features --target=wasm32-unknown-unkno
 # RUSTFLAGS="-D warnings" cargo clippy --all-features -- -D clippy::all
 RUSTFLAGS="-D warnings" cargo clippy --all-features --target=wasm32-unknown-unknown -- -D clippy::all
 
+cargo fmt --all -- --check
+
 cd tests
 ./run_all_tests.sh
