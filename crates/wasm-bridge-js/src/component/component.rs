3012a3ed9726bdd6224c8851f7a0a991aff5c93f
kajacx <kajacx@gmail.com>
2023-07-06 10:51:55 +0200

    Proof of concept load zip files

--- crates/wasm-bridge-js/src/component/component.rs
+++ crates/wasm-bridge-js/src/component/component.rs
@@ -1,5 +1,8 @@
+use std::fmt::Write;
+
 use js_sys::Function;
 use wasm_bindgen::JsValue;
+use zip::ZipArchive;
 
 use crate::{Engine, Result};
 
@@ -9,18 +12,26 @@ pub struct Component {
 }
 
 impl Component {
-    pub fn new(_engine: &Engine, _bytes: &[u8]) -> Result<Self> {
-        // let component = js_sys::eval(load_component)?;
-        // let component: JsValue = await_js_value(component).await?;
+    pub fn new(_engine: &Engine, bytes: &[u8]) -> Result<Self> {
+        let cursor = std::io::Cursor::new(bytes);
+        let mut archive = ZipArchive::new(cursor).unwrap();
 
-        // let instantiate = Reflect::get(&component, &"instantiate".into())?;
-        // let instantiate: Function = instantiate.into();
+        let mut text = String::new();
 
-        // Ok(Self {
-        //     component,
-        //     instantiate,
-        // })
+        for i in 0..archive.len() {
+            let mut file = archive.by_index(i).unwrap();
+            let filename = file.name();
+            writeln!(text, "File name: {}", file.name()).unwrap();
 
-        todo!()
+            if filename.ends_with(".wasm") {
+                let mut file_bytes = Vec::<u8>::new();
+
+                std::io::copy(&mut file, &mut file_bytes).unwrap();
+
+                writeln!(text, "File bytes: {}", file_bytes.len()).unwrap();
+            }
+        }
+
+        todo!("{}", text)
     }
 }
--- tests/skeletons/wit_components/host_js/src/main.rs
+++ tests/skeletons/wit_components/host_js/src/main.rs
@@ -1,8 +1,9 @@
-const PLUGIN_PATH: &'static str = "../../plugin/target/wasm32-unknown-unknown/debug/out-dir/";
+const PLUGIN_ZIP: &'static [u8] =
+    include_bytes!("../../plugin/target/wasm32-unknown-unknown/debug/out-dir.zip");
 
 mod host;
 
 #[wasm_bindgen_test::wasm_bindgen_test]
 fn main() {
-    host::run_test(PLUGIN_PATH.as_bytes()).expect("host_js test should pass")
+    host::run_test(PLUGIN_ZIP).expect("host_js test should pass")
 }
