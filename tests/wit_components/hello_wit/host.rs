b5d06c004595f5d58d4759fc14a2cff7ac8f4401
kajacx <kajacx@gmail.com>
2023-07-08 15:18:00 +0200

    Add import string test

--- tests/wit_components/hello_wit/host.rs
+++ tests/wit_components/hello_wit/host.rs
@@ -12,21 +12,33 @@ wasm_bridge::component::bindgen!({
     world: "test-world",
 });
 
+struct HostData {}
+
+impl TestWorldImports for HostData {
+    fn add_b(&mut self, text: String) -> Result<String> {
+        Ok(text + "b")
+    }
+}
+
 pub fn run_test(component_bytes: &[u8]) -> Result<()> {
     let mut config = Config::new();
     config.wasm_component_model(true);
 
     let engine = Engine::new(&config)?;
-    let mut store = Store::new(&engine, ());
+    let mut store = Store::new(&engine, HostData {});
 
     let component = Component::new(&store.engine(), &component_bytes)?;
 
-    let linker = Linker::new(store.engine());
+    let mut linker = Linker::new(store.engine());
+    TestWorld::add_to_linker(&mut linker, |data| data)?;
 
     let (instance, _) = TestWorld::instantiate(&mut store, &component, &linker)?;
 
-    let result = instance.call_add_hello(&mut store, "world".into())?;
+    let result = instance.call_add_hello(&mut store, "world")?;
     assert_eq!(result, "Hello world");
+
+    let result = instance.call_add_abc(&mut store, "Hello ")?;
+    assert_eq!(result, "Hello abc");
 
     Ok(())
 }
--- tests/wit_components/hello_wit/plugin.rs
+++ tests/wit_components/hello_wit/plugin.rs
@@ -9,6 +9,12 @@ impl TestWorld for Plugin {
     fn add_hello(text: String) -> String {
         format!("Hello {text}")
     }
+
+    fn add_abc(text: String) -> String {
+        let text = text + "a";
+        let text = add_b(&text);
+        text + "c"
+    }
 }
 
 export_test_world!(Plugin);
