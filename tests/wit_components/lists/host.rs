4ee8a1b3e69d48ee0b9bff25d10f6d26db138240
kajacx <kajacx@gmail.com>
2023-07-10 19:48:37 +0200

    Add lists tests for "primitive" types

--- tests/wit_components/lists/host.rs
+++ tests/wit_components/lists/host.rs
@@ -15,6 +15,36 @@ impl TestWorldImports for HostData {
         numbers.push(a);
         Ok(numbers)
     }
+
+    fn push_s64(&mut self, mut numbers: Vec<i64>, a: i64) -> Result<Vec<i64>> {
+        numbers.push(a);
+        Ok(numbers)
+    }
+
+    fn push_u32(&mut self, mut numbers: Vec<u32>, a: u32) -> Result<Vec<u32>> {
+        numbers.push(a);
+        Ok(numbers)
+    }
+
+    fn push_u64(&mut self, mut numbers: Vec<u64>, a: u64) -> Result<Vec<u64>> {
+        numbers.push(a);
+        Ok(numbers)
+    }
+
+    fn push_float32(&mut self, mut numbers: Vec<f32>, a: f32) -> Result<Vec<f32>> {
+        numbers.push(a);
+        Ok(numbers)
+    }
+
+    fn push_float64(&mut self, mut numbers: Vec<f64>, a: f64) -> Result<Vec<f64>> {
+        numbers.push(a);
+        Ok(numbers)
+    }
+
+    fn push_string(&mut self, mut numbers: Vec<String>, a: String) -> Result<Vec<String>> {
+        numbers.push(a);
+        Ok(numbers)
+    }
 }
 
 pub fn run_test(component_bytes: &[u8]) -> Result<()> {
@@ -33,6 +63,32 @@ pub fn run_test(component_bytes: &[u8]) -> Result<()> {
 
     let result = instance.call_push_s32s(&mut store, &[-10, 200], 3, 4)?;
     assert_eq!(result, vec![-10, 200, 3, 4]);
+
+    let result = instance.call_push_s64s(&mut store, &[-10_000_000_000, 200_000_000_000], 3, 4)?;
+    assert_eq!(result, vec![-10_000_000_000, 200_000_000_000, 3, 4]);
+
+    let result = instance.call_push_u32s(&mut store, &[10, u32::MAX - 10], 3, 4)?;
+    assert_eq!(result, vec![10, u32::MAX - 10, 3, 4]);
+
+    let result = instance.call_push_u64s(&mut store, &[10, u64::MAX - 10], 3, 4)?;
+    assert_eq!(result, vec![10, u64::MAX - 10, 3, 4]);
+
+    let result = instance.call_push_float32s(&mut store, &[5.5, -10.25], 3.0, 4.0)?;
+    assert_eq!(result, vec![5.5, -10.25, 3.0, 4.0]);
+
+    let result = instance.call_push_float64s(&mut store, &[5.5, -10.25], 3.0, 4.0)?;
+    assert_eq!(result, vec![5.5, -10.25, 3.0, 4.0]);
+
+    let result = instance.call_push_strings(&mut store, &["hello", "world"], "three", "four")?;
+    assert_eq!(
+        result,
+        vec![
+            "hello".to_owned(),
+            "world".to_owned(),
+            "three".to_owned(),
+            "four".to_owned()
+        ]
+    );
 
     Ok(())
 }
--- tests/wit_components/lists/plugin.rs
+++ tests/wit_components/lists/plugin.rs
@@ -11,6 +11,46 @@ impl TestWorld for Plugin {
         let numbers = push_s32(&numbers, b);
         numbers
     }
+
+    fn push_s64s(numbers: Vec<i64>, a: i64, b: i64) -> Vec<i64> {
+        let numbers = push_s64(&numbers, a);
+        let numbers = push_s64(&numbers, b);
+        numbers
+    }
+
+    fn push_u32s(numbers: Vec<u32>, a: u32, b: u32) -> Vec<u32> {
+        let numbers = push_u32(&numbers, a);
+        let numbers = push_u32(&numbers, b);
+        numbers
+    }
+
+    fn push_u64s(numbers: Vec<u64>, a: u64, b: u64) -> Vec<u64> {
+        let numbers = push_u64(&numbers, a);
+        let numbers = push_u64(&numbers, b);
+        numbers
+    }
+
+    fn push_float32s(numbers: Vec<f32>, a: f32, b: f32) -> Vec<f32> {
+        let numbers = push_float32(&numbers, a);
+        let numbers = push_float32(&numbers, b);
+        numbers
+    }
+
+    fn push_float64s(numbers: Vec<f64>, a: f64, b: f64) -> Vec<f64> {
+        let numbers = push_float64(&numbers, a);
+        let numbers = push_float64(&numbers, b);
+        numbers
+    }
+
+    fn push_strings(strings: Vec<String>, a: String, b: String) -> Vec<String> {
+        let strings = strings.iter().map(String::as_str).collect::<Vec<_>>();
+        let strings = push_string(&strings, &a);
+
+        let strings = strings.iter().map(String::as_str).collect::<Vec<_>>();
+        let strings = push_string(&strings, &b);
+
+        strings
+    }
 }
 
 export_test_world!(Plugin);
