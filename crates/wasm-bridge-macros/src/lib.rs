7d50057a125f67549cd8340aca70445847f424c0
kajacx <kajacx@gmail.com>
2023-07-12 14:56:36 +0200

    Refactor macros slightly

--- crates/wasm-bridge-macros/src/lib.rs
+++ crates/wasm-bridge-macros/src/lib.rs
@@ -1,71 +1,44 @@
 use std::str::FromStr;
 
 use regex::Regex;
-use syn::{parse_macro_input, DeriveInput, Error};
 
 mod bindgen;
 mod component;
+mod original;
 
 #[proc_macro_derive(Lift, attributes(component))]
 pub fn lift(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
-    component::expand(
-        &component::LiftExpander,
-        &parse_macro_input!(input as DeriveInput),
-    )
-    .unwrap_or_else(Error::into_compile_error)
-    .into()
+    replace_namespace(original::lift(input))
 }
 
 #[proc_macro_derive(Lower, attributes(component))]
 pub fn lower(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
-    component::expand(
-        &component::LowerExpander,
-        &parse_macro_input!(input as DeriveInput),
-    )
-    .unwrap_or_else(Error::into_compile_error)
-    .into()
+    replace_namespace(original::lower(input))
 }
 
 #[proc_macro_derive(ComponentType, attributes(component))]
 pub fn component_type(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
-    component::expand(
-        &component::ComponentTypeExpander,
-        &parse_macro_input!(input as DeriveInput),
-    )
-    .unwrap_or_else(Error::into_compile_error)
-    .into()
+    replace_namespace(original::component_type(input))
 }
 
 #[proc_macro]
 pub fn flags(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
-    component::expand_flags(&parse_macro_input!(input as component::Flags))
-        .unwrap_or_else(Error::into_compile_error)
-        .into()
+    replace_namespace(original::flags(input))
 }
 
-fn bindgen(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
-    let stream: proc_macro::TokenStream =
-        bindgen::expand(&parse_macro_input!(input as bindgen::Config))
-            .unwrap_or_else(Error::into_compile_error)
-            .into();
+#[proc_macro]
+pub fn bindgen_sys(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+    let as_string = replace_namespace(original::bindgen(input));
 
-    let as_string = stream.to_string();
-
-    // Replace wasmtime:: package path
-    let regex = Regex::new("wasmtime\\s*::").unwrap();
-    let as_string = regex.replace_all(&as_string, "wasm_bridge::");
+    // TODO: this should not be needed
+    let as_string = format!("mod wasmtime {{ pub use wasm_bridge::*; }}\n\n{as_string}");
 
     proc_macro::TokenStream::from_str(&as_string).unwrap()
 }
 
 #[proc_macro]
-pub fn bindgen_sys(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
-    bindgen(input)
-}
-
-#[proc_macro]
 pub fn bindgen_js(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
-    let as_string = bindgen(input).to_string();
+    let as_string = replace_namespace(original::bindgen(input)).to_string();
 
     // Clone exported function
     let regex = Regex::new("\\*\\s*__exports\\.typed_func([^?]*)\\?\\.func\\(\\)").unwrap();
@@ -87,6 +60,18 @@ pub fn bindgen_js(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     let as_string = regex.replace_all(&as_string, "add_root_to_linker<T: 'static");
 
     // eprintln!("{as_string}");
+
+    proc_macro::TokenStream::from_str(&as_string).unwrap()
+}
+
+fn replace_namespace(stream: proc_macro::TokenStream) -> proc_macro::TokenStream {
+    let as_string = stream.to_string();
+
+    // Replace wasmtime:: package path with wasm_bridge::
+    let regex = Regex::new("wasmtime[^:]*::").unwrap();
+    let as_string = regex.replace_all(&as_string, "wasm_bridge::");
+
+    eprintln!("REPLACED: {as_string}");
 
     proc_macro::TokenStream::from_str(&as_string).unwrap()
 }
---
+++ crates/wasm-bridge-macros/src/original.rs
@@ -0,0 +1,47 @@
+use syn::{parse_macro_input, DeriveInput, Error};
+
+mod bindgen {
+    pub use crate::bindgen::*;
+}
+mod component {
+    pub use crate::component::*;
+}
+
+pub fn lift(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+    component::expand(
+        &component::LiftExpander,
+        &parse_macro_input!(input as DeriveInput),
+    )
+    .unwrap_or_else(Error::into_compile_error)
+    .into()
+}
+
+pub fn lower(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+    component::expand(
+        &component::LowerExpander,
+        &parse_macro_input!(input as DeriveInput),
+    )
+    .unwrap_or_else(Error::into_compile_error)
+    .into()
+}
+
+pub fn component_type(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+    component::expand(
+        &component::ComponentTypeExpander,
+        &parse_macro_input!(input as DeriveInput),
+    )
+    .unwrap_or_else(Error::into_compile_error)
+    .into()
+}
+
+pub fn flags(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+    component::expand_flags(&parse_macro_input!(input as component::Flags))
+        .unwrap_or_else(Error::into_compile_error)
+        .into()
+}
+
+pub fn bindgen(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+    bindgen::expand(&parse_macro_input!(input as bindgen::Config))
+        .unwrap_or_else(Error::into_compile_error)
+        .into()
+}
