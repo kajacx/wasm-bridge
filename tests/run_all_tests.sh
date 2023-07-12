9289a440a69a2b6ddd1023d80d454a4f02f02c6e
kajacx <kajacx@gmail.com>
2023-07-12 12:11:48 +0200

    Re-structure no bindgen tests

--- tests/run_all_tests.sh
+++ tests/run_all_tests.sh
@@ -1,47 +1,10 @@
 #!/usr/bin/sh
+set -e
 
 for test in no_bindgen/*; do
-  # create the instance folder if it doesn't exist
-  mkdir -p instance
+  test=$(echo "$test" | sed 's#no_bindgen/##')
 
-  # copy the "no_bindgen" skeleton
-  cp -r skeletons/no_bindgen/plugin ./instance
-  cp -r skeletons/no_bindgen/host_sys ./instance
-  cp -r skeletons/no_bindgen/host_js ./instance
-
-  # copy the plugin code
-  cp $test/plugin.rs instance/plugin/src/lib.rs
-
-  # build the plugin
-  cd instance/plugin && cargo rustc --target=wasm32-unknown-unknown -- -C target-feature=+multivalue && cd ../..
-  if [ $? -ne 0 ]; then
-    echo
-    echo "Oh no, there is an error in the $test plugin."
-    echo "Inspect the instance for more detail."
-    exit 1
-  fi
-
-  # copy the host code
-  cp $test/host.rs instance/host_sys/src/host.rs
-  cp $test/host.rs instance/host_js/src/host.rs
-
-  # run the sys host test
-  cd instance/host_sys && cargo run && cd ../..
-  if [ $? -ne 0 ]; then
-    echo
-    echo "Oh no, there is an error in the $test sys host."
-    echo "Inspect the instance for more detail."
-    exit 1
-  fi
-
-  # run the js host test
-  cd instance/host_js && wasm-pack test --node && cd ../..
-  if [ $? -ne 0 ]; then
-    echo
-    echo "Oh no, there is an error in the $test js host."
-    echo "Inspect the instance for more detail."
-    exit 1
-  fi
+  sh skeletons/no_bindgen/run_test.sh "$test"
 done
 
 sh run_wit_tests.sh
---
+++ tests/skeletons/no_bindgen/run_test.sh
@@ -0,0 +1,48 @@
+#!/use/bin/sh
+
+# Run from the "tests" folder and pass the instance name as an argument
+test="no_bindgen/$1"
+
+# create the instances/no_bindgen folder if it doesn't exist
+mkdir -p instances/no_bindgen
+
+# copy the "no_bindgen" skeleton
+cp -r skeletons/no_bindgen/plugin ./instances/no_bindgen
+cp -r skeletons/no_bindgen/host_sys ./instances/no_bindgen
+cp -r skeletons/no_bindgen/host_js ./instances/no_bindgen
+
+# copy the plugin code
+cp $test/plugin.rs instances/no_bindgen/plugin/src/lib.rs
+
+# build the plugin
+cd instances/no_bindgen/plugin && cargo rustc --target=wasm32-unknown-unknown -- -C target-feature=+multivalue && cd ../../..
+if [ $? -ne 0 ]; then
+  echo
+  echo "Oh no, there is an error in the $test plugin."
+  echo "Inspect the instances/no_bindgen for more detail."
+  exit 1
+fi
+
+pwd
+
+# copy the host code
+cp $test/host.rs instances/no_bindgen/host_sys/src/host.rs
+cp $test/host.rs instances/no_bindgen/host_js/src/host.rs
+
+# run the sys host test
+cd instances/no_bindgen/host_sys && cargo run && cd ../../..
+if [ $? -ne 0 ]; then
+  echo
+  echo "Oh no, there is an error in the $test sys host."
+  echo "Inspect the instances/no_bindgen for more detail."
+  exit 1
+fi
+
+# run the js host test
+cd instances/no_bindgen/host_js && wasm-pack test --node && cd ../../..
+if [ $? -ne 0 ]; then
+  echo
+  echo "Oh no, there is an error in the $test js host."
+  echo "Inspect the instances/no_bindgen for more detail."
+  exit 1
+fi
