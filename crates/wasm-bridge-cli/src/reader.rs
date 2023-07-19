7ad1f537752c9f39fef5ba698acf34d9ef1468b7
kajacx <kajacx@gmail.com>
2023-07-19 13:10:24 +0200

    Update cli tool

--- crates/wasm-bridge-cli/src/reader.rs
+++ crates/wasm-bridge-cli/src/reader.rs
@@ -9,20 +9,26 @@ pub fn read_files_from_dir(dir: &Path) -> Result<Vec<ZippableFile>> {
     let mut results = vec![];
 
     for file in source_dir {
-        results.push(read_path_buf(&file?.path())?);
+        results.extend(read_path_buf(&file?.path())?);
     }
 
     Ok(results)
 }
 
-pub fn read_path_buf(path: &PathBuf) -> Result<ZippableFile> {
+pub fn read_path_buf(path: &PathBuf) -> Result<Vec<ZippableFile>> {
     let file_name = path.file_name().context("get file name")?;
     let file_name = file_name.to_str().context("file name is valid utf-8")?;
 
-    let file_bytes = std::fs::read(path)?;
-
-    Ok(ZippableFile {
-        name: file_name.into(),
-        content: file_bytes,
-    })
+    if path.is_file() {
+        let file_bytes = std::fs::read(path)?;
+        Ok(vec![ZippableFile {
+            name: file_name.into(),
+            content: file_bytes,
+        }])
+    } else if path.is_dir() {
+        // TODO: this "flattens" the directory structure, which is not great
+        read_files_from_dir(path)
+    } else {
+        anyhow::bail!("Path is not a file nor a dir: {path:?}")
+    }
 }
--- crates/wasm-bridge-cli/src/transformers/universal.rs
+++ crates/wasm-bridge-cli/src/transformers/universal.rs
@@ -1,6 +1,6 @@
 use std::path::PathBuf;
 
-use anyhow::anyhow;
+use anyhow::{bail, Context};
 
 use crate::reader::read_path_buf;
 
@@ -9,12 +9,14 @@ use super::Transformer;
 pub fn add_original_file(file: PathBuf) -> Transformer {
     Box::new(move |files| {
         if !file.is_file() {
-            return Err(anyhow!(
-                "Pass the WASM component file name as the --universal parameter"
-            ));
+            bail!("Pass the WASM component file name as the --universal parameter");
         }
 
-        let mut component = read_path_buf(&file)?;
+        let mut component = read_path_buf(&file)?
+            .into_iter()
+            .next()
+            .context("Should read original file")?;
+
         component.name = "original_component.wasm".into();
 
         files.push(component);
