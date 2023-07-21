6f2953ff065fbdd315ccb18be37a2de49feaac76
kajacx <kajacx@gmail.com>
2023-07-21 18:17:25 +0200

    Put test instances back into the same folder

--- tests/skeletons/no_bindgen/run_test.sh
+++ tests/skeletons/no_bindgen/run_test.sh
@@ -6,44 +6,44 @@ export RUSTFLAGS="-D warnings"
 # Run from the "tests" folder and pass the instance name as an argument
 test="no_bindgen/$1"
 
-# create the instances/no_bindgen folder if it doesn't exist
-mkdir -p instances/no_bindgen
+# create the instance folder if it doesn't exist
+mkdir -p instance
 
 # copy the "no_bindgen" skeleton
-cp -r skeletons/no_bindgen/guest instances/no_bindgen
-cp -r skeletons/no_bindgen/host_sys instances/no_bindgen
-cp -r skeletons/no_bindgen/host_js instances/no_bindgen
+cp -r skeletons/no_bindgen/guest instance
+cp -r skeletons/no_bindgen/host_sys instance
+cp -r skeletons/no_bindgen/host_js instance
 
 # copy the guest code
-cp $test/guest.rs instances/no_bindgen/guest/src/lib.rs
+cp $test/guest.rs instance/guest/src/lib.rs
 
 # build the guest
-cd instances/no_bindgen/guest && cargo rustc --target=wasm32-unknown-unknown -- -C target-feature=+multivalue && cd ../../..
+cd instance/guest && cargo rustc --target=wasm32-unknown-unknown -- -C target-feature=+multivalue && cd ../..
 if [ $? -ne 0 ]; then
   echo
   echo "Oh no, there is an error in the $test guest."
-  echo "Inspect the instances/no_bindgen for more detail."
+  echo "Inspect the instance folder for more detail."
   exit 1
 fi
 
 # copy the host code
-cp $test/host.rs instances/no_bindgen/host_sys/src/host.rs
-cp $test/host.rs instances/no_bindgen/host_js/src/host.rs
+cp $test/host.rs instance/host_sys/src/host.rs
+cp $test/host.rs instance/host_js/src/host.rs
 
 # run the sys host test
-cd instances/no_bindgen/host_sys && cargo run && cd ../../..
+cd instance/host_sys && cargo run && cd ../..
 if [ $? -ne 0 ]; then
   echo
   echo "Oh no, there is an error in the $test sys host."
-  echo "Inspect the instances/no_bindgen for more detail."
+  echo "Inspect the instance folder for more detail."
   exit 1
 fi
 
 # run the js host test
-cd instances/no_bindgen/host_js && wasm-pack test --node && cd ../../..
+cd instance/host_js && wasm-pack test --node && cd ../..
 if [ $? -ne 0 ]; then
   echo
   echo "Oh no, there is an error in the $test js host."
-  echo "Inspect the instances/no_bindgen for more detail."
+  echo "Inspect the instance folder for more detail."
   exit 1
 fi
--- tests/skeletons/wit_components/run_test.sh
+++ tests/skeletons/wit_components/run_test.sh
@@ -6,50 +6,50 @@ export RUSTFLAGS="-D warnings"
 # Run from the "tests" folder and pass the instance name as an argument
 test="wit_components/$1"
 
-# create the instances/wit_components folder if it doesn't exist
-mkdir -p instances/wit_components
+# create the instance folder if it doesn't exist
+mkdir -p instance
 
 # copy the "wit_components" skeleton
-cp -r skeletons/wit_components/guest instances/wit_components
-cp -r skeletons/wit_components/host_sys instances/wit_components
-cp -r skeletons/wit_components/host_js instances/wit_components
+cp -r skeletons/wit_components/guest instance
+cp -r skeletons/wit_components/host_sys instance
+cp -r skeletons/wit_components/host_js instance
 
 # copy the protocol
-cp $test/protocol.wit instances/wit_components/protocol.wit
+cp $test/protocol.wit instance/protocol.wit
 
 # copy the guest code
-cp $test/guest.rs instances/wit_components/guest/src/lib.rs
+cp $test/guest.rs instance/guest/src/lib.rs
 
 # build the guest
-cd instances/wit_components/guest && cargo rustc --target=wasm32-unknown-unknown -- -C target-feature=+multivalue && \
+cd instance/guest && cargo rustc --target=wasm32-unknown-unknown -- -C target-feature=+multivalue && \
 cd target/wasm32-unknown-unknown/debug && \
 wasm-tools component new wit_components_guest.wasm -o component.wasm && \
-cd ../../../../../..
+cd ../../../../..
 if [ $? -ne 0 ]; then
   echo
   echo "Oh no, there is an error in the $test guest."
-  echo "Inspect the instances/wit_components for more detail."
+  echo "Inspect the instance folder for more detail."
   exit 1
 fi
 
 # copy the host code
-cp $test/host.rs instances/wit_components/host_sys/src/host.rs
-cp $test/host.rs instances/wit_components/host_js/src/host.rs
+cp $test/host.rs instance/host_sys/src/host.rs
+cp $test/host.rs instance/host_js/src/host.rs
 
 # run the sys host test
-cd instances/wit_components/host_sys && cargo run && cd ../../..
+cd instance/host_sys && cargo run && cd ../..
 if [ $? -ne 0 ]; then
   echo
   echo "Oh no, there is an error in the $test sys host."
-  echo "Inspect the instances/wit_components for more detail."
+  echo "Inspect the instance folder for more detail."
   exit 1
 fi
 
 # run the js host test
-cd instances/wit_components/host_js && wasm-pack test --node && cd ../../..
+cd instance/host_js && wasm-pack test --node && cd ../..
 if [ $? -ne 0 ]; then
   echo
   echo "Oh no, there is an error in the $test js host."
-  echo "Inspect the instances/wit_components for more detail."
+  echo "Inspect the instance folder for more detail."
   exit 1
 fi
