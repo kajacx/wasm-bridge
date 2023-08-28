7c67b4792a1a6b6dbe9b12824982f665cc469d1f
Tei Leelo Roberts <ten3roberts@gmail.com>
2023-08-28 15:50:07 +0200

    feat: `wit_components/run_examples.sh`

---
+++ examples/wit_components/run_examples.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -e
+
+if [[ $1 ]]; then
+    echo "-> Building $1"
+    EXAMPLES=$1
+else
+    EXAMPLES=`ls `
+fi
+
+
+# Build the guest
+for example in $EXAMPLES; do
+    if [[ ! -d $example ]]; then
+        continue
+    fi
+
+    echo "-> Running $example"
+    cargo component build --target wasm32-unknown-unknown -p example-$example-guest
+    cargo run -p example-$example-host
+done
+
+echo "Done building"
