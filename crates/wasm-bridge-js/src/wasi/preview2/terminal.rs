ed437e103888dbe60d93c5a8ddaf3a9c6d88b99f
Tei Leelo Roberts <ten3roberts@gmail.com>
2023-09-01 12:09:11 +0200

    fix: terminal world

--- crates/wasm-bridge-js/src/wasi/preview2/terminal.rs
+++ crates/wasm-bridge-js/src/wasi/preview2/terminal.rs
@@ -5,19 +5,15 @@ use crate::{component::Linker, Result, StoreContextMut};
 use super::{stream, WasiView};
 
 pub(crate) fn add_to_linker<T: 'static + WasiView>(linker: &mut Linker<T>) -> Result<()> {
-    linker
-        .instance("wasi:filesystem/terminal-input")?
-        .func_wrap(
-            "drop-terminal-input",
-            |_data: StoreContextMut<T>, (_this,): (u32,)| -> Result<()> { Ok(()) },
-        )?;
+    linker.instance("wasi:cli/terminal-input")?.func_wrap(
+        "drop-terminal-input",
+        |_data: StoreContextMut<T>, (_this,): (u32,)| -> Result<()> { Ok(()) },
+    )?;
 
-    linker
-        .instance("wasi:filesystem/terminal-output")?
-        .func_wrap(
-            "drop-terminal-output",
-            |_data: StoreContextMut<T>, (_this,): (u32,)| -> Result<()> { Ok(()) },
-        )?;
+    linker.instance("wasi:cli/terminal-output")?.func_wrap(
+        "drop-terminal-output",
+        |_data: StoreContextMut<T>, (_this,): (u32,)| -> Result<()> { Ok(()) },
+    )?;
 
     Ok(())
 }
