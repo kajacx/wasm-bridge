547d5258c9cde0720461210c33a6cbc6260d6ee7
Tei Leelo Roberts <ten3roberts@gmail.com>
2023-09-05 09:38:54 +0200

    chore! remove wasi js shim
    This creates a mostly stubbed js fallback for functions, which leads to
    unexpected errors such as `e is not iterable`, and will additionally
    poke more holes in the sandbox than expected.

--- crates/wasm-bridge-js/src/wasi/preview2/command.rs
+++ crates/wasm-bridge-js/src/wasi/preview2/command.rs
@@ -5,16 +5,9 @@ use crate::component::Linker;
 use crate::wasi::preview2::{clocks, WasiView};
 use crate::{Result, StoreContextMut};
 
-use super::stdio::{STDERR_IDENT, STDIN_IDENT, STDOUT_IDENT};
 use super::{environment, filesystem, preopens, stdio, streams, terminal};
 
-static WASI_IMPORTS_STR: &str =
-    include_str!("../../../../../resources/transformed/preview2-shim/bundled.js");
-
 pub fn add_to_linker<T: WasiView + 'static>(linker: &mut Linker<T>) -> Result<()> {
-    // Default imports
-    // linker.set_wasi_imports(get_imports());
-
     // Overrides
     // linker.instance("wasi:io/streams")?.func_wrap(
     //     "read",
@@ -83,26 +76,4 @@ pub fn add_to_linker<T: WasiView + 'static>(linker: &mut Linker<T>) -> Result<()
     streams::add_to_linker(linker)?;
 
     Ok(())
-}
-
-/// Fills the wasi imports by the browser shim
-fn get_imports() -> Object {
-    let imports = js_sys::eval(WASI_IMPORTS_STR).expect("eval bundled wasi imports");
-
-    assert!(
-        imports.is_object(),
-        "wasi imports must be an object, {imports:#?}"
-    );
-
-    imports.into()
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[wasm_bindgen_test::wasm_bindgen_test]
-    fn should_get_imports() {
-        let _ = get_imports();
-    }
 }
