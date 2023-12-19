42c0f36004c31ae4108156087741dd34cddc6cdb
kajacx <kajacx@gmail.com>
2023-12-19 19:46:08 +0100

    lift compiles

--- crates/wasm-bridge-js/src/direct_bytes/lift.rs
+++ crates/wasm-bridge-js/src/direct_bytes/lift.rs
@@ -3,7 +3,11 @@ use anyhow::Result;
 use super::SizeDescription;
 
 pub trait Lift: SizeDescription + Sized {
-    fn read_from<T: ReadableMemory>(memory: T, addr: usize) -> Result<Self>;
+    type Abi;
+
+    fn from_abi<M: ReadableMemory>(abi: Self::Abi, memory: M) -> Result<Self>;
+
+    fn read_from<M: ReadableMemory>(slice: &[u8], memory: M) -> Result<Self>;
 }
 
 pub trait ReadableMemory {
@@ -12,7 +16,13 @@ pub trait ReadableMemory {
     fn read_to_vec(&self, addr: usize, len: usize) -> Vec<u8> {
         // TODO: could do uninit memory with unsafe code
         let mut vec: Vec<u8> = (0..len).map(|_| 0).collect();
-        self.read_slice(addr, &mut vec);
+        self.read_to_slice(addr, &mut vec);
         vec
+    }
+}
+
+impl<M: ReadableMemory> ReadableMemory for &M {
+    fn read_to_slice(&self, addr: usize, target: &mut [u8]) {
+        M::read_to_slice(self, addr, target)
     }
 }
--- crates/wasm-bridge-js/src/direct_bytes/lift_impls.rs
+++ crates/wasm-bridge-js/src/direct_bytes/lift_impls.rs
@@ -3,24 +3,50 @@ use anyhow::Result;
 use super::*;
 
 impl Lift for i32 {
-    fn read_from<M: ReadableMemory>(memory: M, addr: usize) -> Result<Self> {
-        let mut le_bytes = [0u8; 4];
-        memory.read_slice(addr, &mut le_bytes);
-        Ok(i32::from_le_bytes(le_bytes))
+    type Abi = i32;
+
+    fn from_abi<M: ReadableMemory>(abi: Self::Abi, _memory: M) -> Result<Self> {
+        Ok(abi)
+    }
+
+    fn read_from<M: ReadableMemory>(slice: &[u8], _memory: M) -> Result<Self> {
+        Ok(i32::from_le_bytes(slice.try_into()?))
     }
 }
 
 impl Lift for u32 {
-    fn read_from<M: ReadableMemory>(memory: M, addr: usize) -> Result<Self> {
-        let mut le_bytes = [0u8; 4];
-        memory.read_slice(addr, &mut le_bytes);
-        Ok(u32::from_le_bytes(le_bytes))
+    type Abi = i32;
+
+    fn from_abi<M: ReadableMemory>(abi: Self::Abi, _memory: M) -> Result<Self> {
+        Ok(abi as _)
+    }
+
+    fn read_from<M: ReadableMemory>(slice: &[u8], _memory: M) -> Result<Self> {
+        Ok(u32::from_le_bytes(slice.try_into()?))
     }
 }
 
 impl<T: Lift> Lift for Vec<T> {
-    fn read_from<M: ReadableMemory>(memory: M, addr: usize) -> Result<Self> {
-        
-        let data = memory.read_to_vec(addr, T::flat_byte_size() * )
+    type Abi = u32;
+
+    fn from_abi<M: ReadableMemory>(addr: Self::Abi, memory: M) -> Result<Self> {
+        let mut addr_and_len = [0u8; 8];
+        memory.read_to_slice(addr as usize, &mut addr_and_len);
+
+        Self::read_from(&addr_and_len, memory)
+    }
+
+    fn read_from<M: ReadableMemory>(addr_and_len: &[u8], memory: M) -> Result<Self> {
+        let addr = u32::from_le_bytes(addr_and_len[0..4].try_into().unwrap()) as usize;
+        let len = u32::from_le_bytes(addr_and_len[4..8].try_into().unwrap()) as usize;
+
+        let data = memory.read_to_vec(addr, len);
+        let size = T::flat_byte_size();
+
+        let mut result = Vec::with_capacity(len);
+        for i in 0..len {
+            result.push(T::read_from(&data[i * size..(i + 1) * size], &memory)?);
+        }
+        Ok(result)
     }
 }
