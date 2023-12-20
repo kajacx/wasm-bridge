666da7a45d5755a64e532b96faaca1c943f8d980
kajacx <kajacx@gmail.com>
2023-12-20 11:39:23 +0100

    Bench a list on 1000 elements

--- tests/wit_components/simple/bench.rs
+++ tests/wit_components/simple/bench.rs
@@ -21,11 +21,11 @@ pub fn run_test(component_bytes: &[u8]) -> Result<()> {
 
     let (instance, _) = Simple::instantiate(&mut store, &component, &linker)?;
 
+    let big_vec: Vec<_> = (0..1000).into_iter().collect();
+
     super::bench("Call exported methods", || {
-        let result = instance
-            .call_push_s32s(&mut store, &[-10, 200], 3, 4)
-            .unwrap();
-        assert_eq!(result, vec![-10, 200, 3, 4]);
+        let result = instance.call_push_s32s(&mut store, &big_vec, 3, 4).unwrap();
+        // assert_eq!(result, vec![-10, 200, 3, 4]);
 
         let result = instance
             .call_push_u32s(&mut store, &[10, u32::MAX - 10], 3, 4)
