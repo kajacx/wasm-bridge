c7a5ebd8a107929494f193581f6d568ed3fab1b3
kajacx <kajacx@gmail.com>
2023-12-21 11:49:54 +0100

    Prepare layout calculations

--- crates/wasm-bridge-js/src/direct_bytes/size_description.rs
+++ crates/wasm-bridge-js/src/direct_bytes/size_description.rs
@@ -1,42 +1,73 @@
 pub trait SizeDescription {
+    type StructLayout;
+
     /// Alignment in bytes
     fn alignment() -> usize;
 
-    /// How many bytes would a field of this type take in a struct. Must be a multiple of alignment.
+    /// How many bytes would a field of this type take in a struct or a vec.
+    /// Must be a multiple of alignment.
     fn flat_byte_size() -> usize;
+
+    fn layout() -> Self::StructLayout;
+}
+
+type SimpleStructLayout = [usize; 3];
+
+fn simple_layout(flat_byte_size: usize) -> SimpleStructLayout {
+    [0, flat_byte_size, flat_byte_size]
 }
 
 impl SizeDescription for i32 {
+    type StructLayout = SimpleStructLayout;
+
     fn alignment() -> usize {
         4
     }
 
     fn flat_byte_size() -> usize {
         4
+    }
+
+    fn layout() -> Self::StructLayout {
+        simple_layout(Self::flat_byte_size())
     }
 }
 
 impl SizeDescription for u32 {
+    type StructLayout = SimpleStructLayout;
+
     fn alignment() -> usize {
         4
     }
 
     fn flat_byte_size() -> usize {
         4
+    }
+
+    fn layout() -> Self::StructLayout {
+        simple_layout(Self::flat_byte_size())
     }
 }
 
 impl<T: SizeDescription> SizeDescription for &[T] {
+    type StructLayout = SimpleStructLayout;
+
     fn alignment() -> usize {
         4
     }
 
     fn flat_byte_size() -> usize {
         8
+    }
+
+    fn layout() -> Self::StructLayout {
+        simple_layout(Self::flat_byte_size())
     }
 }
 
 impl<T: SizeDescription> SizeDescription for Vec<T> {
+    type StructLayout = SimpleStructLayout;
+
     fn alignment() -> usize {
         4
     }
@@ -44,50 +75,98 @@ impl<T: SizeDescription> SizeDescription for Vec<T> {
     fn flat_byte_size() -> usize {
         8
     }
+
+    fn layout() -> Self::StructLayout {
+        simple_layout(Self::flat_byte_size())
+    }
 }
 
 impl SizeDescription for () {
+    type StructLayout = [usize; 1];
+
     fn alignment() -> usize {
         1
     }
+
     fn flat_byte_size() -> usize {
         0
     }
+
+    fn layout() -> Self::StructLayout {
+        [0]
+    }
 }
 
-// TODO: probably remove this
 impl<T: SizeDescription> SizeDescription for (T,) {
+    type StructLayout = T::StructLayout;
+
     fn alignment() -> usize {
         T::alignment()
     }
+
     fn flat_byte_size() -> usize {
         T::flat_byte_size()
+    }
+
+    fn layout() -> Self::StructLayout {
+        T::layout()
     }
 }
 
 impl<T: SizeDescription, U: SizeDescription> SizeDescription for (T, U) {
+    type StructLayout = [usize; 5];
+
     fn alignment() -> usize {
         usize::max(T::alignment(), U::alignment())
     }
 
     fn flat_byte_size() -> usize {
+        Self::layout()[4]
+    }
+
+    fn layout() -> Self::StructLayout {
         let align = Self::alignment();
-        next_multiple_of(T::flat_byte_size(), align) + next_multiple_of(U::flat_byte_size(), align)
+        let start = 0;
+
+        let start0 = next_multiple_of(start, align);
+        let end0 = start0 + T::flat_byte_size();
+
+        let start1 = next_multiple_of(end0, align);
+        let end1 = start1 + U::flat_byte_size();
+
+        let end = next_multiple_of(end1, align);
+
+        [start0, end0, start1, end1, end]
     }
 }
 
 impl<T: SizeDescription, U: SizeDescription, V: SizeDescription> SizeDescription for (T, U, V) {
+    type StructLayout = [usize; 7];
+
     fn alignment() -> usize {
         usize::max(usize::max(T::alignment(), U::alignment()), V::alignment())
     }
 
     fn flat_byte_size() -> usize {
-        let align = Self::alignment();
+        Self::layout()[6]
+    }
 
-        // FIXME: again, this is wrong
-        next_multiple_of(T::flat_byte_size(), align)
-            + next_multiple_of(U::flat_byte_size(), align)
-            + next_multiple_of(V::flat_byte_size(), align)
+    fn layout() -> Self::StructLayout {
+        let align = Self::alignment();
+        let start = 0;
+
+        let start0 = next_multiple_of(start, align);
+        let end0 = start0 + T::flat_byte_size();
+
+        let start1 = next_multiple_of(end0, align);
+        let end1 = start1 + U::flat_byte_size();
+
+        let start2 = next_multiple_of(end1, align);
+        let end2 = start2 + V::flat_byte_size();
+
+        let end = next_multiple_of(end1, align);
+
+        [start0, end0, start1, end1, start2, end2, end]
     }
 }
 
