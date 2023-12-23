d6094044f10e8c6f78df790310b489164f8ba41b
kajacx <kajacx@gmail.com>
2023-12-23 09:44:15 +0100

    Test on js optimized version as well

---
+++ tests/skeletons/wit_components/host_js_opt/src/main.rs
@@ -0,0 +1,9 @@
+const GUEST_BYTES: &'static [u8] =
+    include_bytes!("../../guest/target/wasm32-unknown-unknown/release/wit_components_guest.wasm");
+
+mod host;
+
+#[wasm_bindgen_test::wasm_bindgen_test]
+fn main() {
+    host::run_test(GUEST_BYTES).expect("host_js_opt test should pass")
+}
--- tests/skeletons/wit_components/run_test.sh
+++ tests/skeletons/wit_components/run_test.sh
@@ -12,10 +12,12 @@ fi
 # copy the "wit_components" skeleton
 cp -r skeletons/wit_components/host_sys instance
 cp -r skeletons/wit_components/host_js instance
+cp -r skeletons/wit_components/host_js_opt instance
 
 # copy the host code
 cp $test/host.rs instance/host_sys/src/host.rs
 cp $test/host.rs instance/host_js/src/host.rs
+cp $test/host.rs instance/host_js_opt/src/host.rs
 
 # run the sys host test
 cd instance/host_sys && cargo run && cd ../..
@@ -34,3 +36,13 @@ if [ $? -ne 0 ]; then
   echo "Inspect the instance folder for more detail."
   exit 1
 fi
+
+# run the js host optimized test
+cd instance/host_js_opt && wasm-pack test --node && cd ../..
+if [ $? -ne 0 ]; then
+  echo
+  echo "Oh no, there is an error in the $test js optimized host."
+  echo "Inspect the instance folder for more detail."
+  exit 1
+fi
+
