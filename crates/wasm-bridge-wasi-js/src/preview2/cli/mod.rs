be7837773e083f77d8ff88415c934c4911602a9a
kajacx <kajacx@gmail.com>
2023-12-28 12:06:53 +0100

    Add dummy terminal io

--- crates/wasm-bridge-wasi-js/src/preview2/cli/mod.rs
+++ crates/wasm-bridge-wasi-js/src/preview2/cli/mod.rs
@@ -8,6 +8,9 @@ mod exit;
 mod stderr;
 mod stdin;
 mod stdout;
+mod terminal_stderr;
+mod terminal_stdin;
+mod terminal_stdout;
 
 pub(crate) fn add_to_linker<T: WasiView + 'static>(linker: &mut Linker<T>) -> Result<()> {
     environment::add_to_linker(linker)?;
@@ -15,5 +18,8 @@ pub(crate) fn add_to_linker<T: WasiView + 'static>(linker: &mut Linker<T>) -> Re
     stderr::add_to_linker(linker)?;
     stdout::add_to_linker(linker)?;
     stdin::add_to_linker(linker)?;
+    terminal_stderr::add_to_linker(linker)?;
+    terminal_stdout::add_to_linker(linker)?;
+    terminal_stdin::add_to_linker(linker)?;
     Ok(())
 }
---
+++ crates/wasm-bridge-wasi-js/src/preview2/cli/terminal_stdout.rs
@@ -0,0 +1,19 @@
+use wasm_bridge::component::Linker;
+use wasm_bridge::Result;
+
+use crate::preview2::WasiView;
+
+wasm_bridge::component::bindgen!({
+    path: "src/preview2/wits/terminal_stdout.wit",
+    world: "exports"
+});
+
+impl<T: WasiView> wasi::cli::terminal_stdout::Host for T {
+    fn get_terminal_stdout(&mut self) -> Result<()> {
+        Ok(())
+    }
+}
+
+pub(crate) fn add_to_linker<T: WasiView + 'static>(linker: &mut Linker<T>) -> Result<()> {
+    Exports::add_to_linker(linker, |d| d)
+}
