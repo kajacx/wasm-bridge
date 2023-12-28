155c5ca85af125cfa1b152f8b7e14d1739613d0d
kajacx <kajacx@gmail.com>
2023-12-28 09:31:09 +0100

    Use wit bindgen for clocks

--- crates/wasm-bridge-wasi-js/src/preview2/clocks/monotonic_clock.rs
+++ crates/wasm-bridge-wasi-js/src/preview2/clocks/monotonic_clock.rs
@@ -27,22 +27,41 @@ pub(crate) fn default_monotonic_clock() -> impl HostMonotonicClock {
     JsClock
 }
 
-pub(crate) fn add_to_linker<T: WasiView + 'static>(linker: &mut Linker<T>) -> Result<()> {
-    linker.instance("wasi:clocks/monotonic-clock")?.func_wrap(
-        "resolution",
-        |data: StoreContextMut<T>, (): ()| {
-            let clock = data.ctx().monotonic_clock();
-            Ok(clock.resolution())
-        },
-    )?;
+wasm_bridge::component::bindgen!({
+    path: "src/preview2/clocks/monotonic_clock.wit",
+    world: "exports"
+});
 
-    linker.instance("wasi:clocks/monotonic-clock")?.func_wrap(
-        "now",
-        |data: StoreContextMut<T>, (): ()| {
-            let clock = data.ctx().monotonic_clock();
-            Ok(clock.now())
-        },
-    )?;
+impl<T: WasiView> wasi::clocks::monotonic_clock::Host for T {
+    fn resolution(&mut self) -> Result<u64> {
+        Ok(self.ctx().monotonic_clock().resolution())
+    }
 
-    Ok(())
+    fn now(&mut self) -> Result<u64> {
+        Ok(self.ctx().monotonic_clock().now())
+    }
 }
+
+pub(crate) fn add_to_linker<T: WasiView>(linker: &mut Linker<T>) -> Result<()> {
+    Exports::add_to_linker(linker, |d| d)
+}
+
+// pub(crate) fn add_to_linker<T: WasiView + 'static>(linker: &mut Linker<T>) -> Result<()> {
+//     linker.instance("wasi:clocks/monotonic-clock")?.func_wrap(
+//         "resolution",
+//         |data: StoreContextMut<T>, (): ()| {
+//             let clock = data.ctx().monotonic_clock();
+//             Ok(clock.resolution())
+//         },
+//     )?;
+
+//     linker.instance("wasi:clocks/monotonic-clock")?.func_wrap(
+//         "now",
+//         |data: StoreContextMut<T>, (): ()| {
+//             let clock = data.ctx().monotonic_clock();
+//             Ok(clock.now())
+//         },
+//     )?;
+
+//     Ok(())
+// }
--- crates/wasm-bridge-wasi-js/src/preview2/clocks/wall_clock.rs
+++ crates/wasm-bridge-wasi-js/src/preview2/clocks/wall_clock.rs
@@ -5,6 +5,8 @@ use wasm_bridge::{Result, StoreContextMut};
 
 use crate::preview2::WasiView;
 
+use self::wasi::clocks::wall_clock::WallTime;
+
 pub trait HostWallClock: Send + Sync {
     fn resolution(&self) -> Duration;
     fn now(&self) -> Duration;
@@ -30,27 +32,54 @@ pub(crate) fn real_wall_clock() -> impl HostWallClock {
     JsClock
 }
 
-pub(crate) fn add_to_linker<T: WasiView + 'static>(linker: &mut Linker<T>) -> Result<()> {
-    linker.instance("wasi:clocks/wall-clock")?.func_wrap(
-        "resolution",
-        |data: StoreContextMut<T>, (): ()| {
-            let clock = data.ctx().wall_clock();
-            Ok(clock.resolution().as_nanos() as u64)
-        },
-    )?;
+wasm_bridge::component::bindgen!({
+    path: "src/preview2/clocks/wall_clock.wit",
+    world: "exports"
+});
 
-    linker.instance("wasi:clocks/wall-clock")?.func_wrap(
-        "now",
-        |data: StoreContextMut<T>, (): ()| {
-            let now = data.ctx().wall_clock().now();
-            // Ok(WallTime {
-            //     seconds: now.as_secs(),
-            //     nanoseconds: now.subsec_nanos(),
-            // })
-            // TODO: fix properly
-            Ok((now.as_secs(), now.subsec_nanos()))
-        },
-    )?;
+impl<T: WasiView> wasi::clocks::wall_clock::Host for T {
+    fn resolution(&mut self) -> Result<u64> {
+        Ok(self.ctx().wall_clock().resolution().as_nanos() as u64)
+    }
 
-    Ok(())
+    fn now(&mut self) -> Result<wasi::clocks::wall_clock::WallTime> {
+        let now = self.ctx().wall_clock().now();
+        Ok(WallTime {
+            seconds: now.as_secs(),
+            nanoseconds: now.subsec_nanos(),
+        })
+    }
 }
+
+pub(crate) fn add_to_linker<T: WasiView>(linker: &mut Linker<T>) -> Result<()> {
+    Exports::add_to_linker(linker, |d| d)
+}
+
+// pub(crate) fn add_to_linker<T: WasiView + 'static>(linker: &mut Linker<T>) -> Result<()> {
+//     linker.instance("wasi:clocks/wall-clock")?.func_wrap(
+//         "resolution",
+//         |data: StoreContextMut<T>, (): ()| {
+//             let clock = data.ctx().wall_clock();
+//             Ok(clock.resolution().as_nanos() as u64)
+//         },
+//     )?;
+
+//     linker.instance("wasi:clocks/wall-clock")?.func_wrap(
+//         "now",
+//         |data: StoreContextMut<T>, (): ()| {
+//             let now = data.ctx().wall_clock().now();
+//             Ok(WallTime {
+//                 seconds: now.as_secs(),
+//                 nanoseconds: now.subsec_nanos(),
+//             })
+//         },
+//     )?;
+
+//     Ok(())
+// }
+
+// #[derive(wasm_bridge_macros::SizeDescription, wasm_bridge_macros::LowerJs)]
+// struct WallTime {
+//     seconds: u64,
+//     nanoseconds: u32,
+// }
