db0e7f0cf2e5aabe5563b6cb7e727bdfa6f27530
kajacx <kajacx@gmail.com>
2024-01-01 15:46:43 +0100

    Run wasi tests as tests

---
+++ tests/skeletons/wasi_components/host_sys/src/lib.rs
@@ -0,0 +1,11 @@
+const GUEST_BYTES: &'static [u8] =
+    include_bytes!("../../guest/target/wasm32-wasi/release/wasi_components_guest.wasm");
+
+mod host;
+
+#[tokio::test(flavor = "current_thread")]
+async fn test() {
+    host::run_test(GUEST_BYTES)
+        .await
+        .expect("host_sys test should pass");
+}
--- tests/skeletons/wasi_components/run_test.sh
+++ tests/skeletons/wasi_components/run_test.sh
@@ -38,7 +38,7 @@ if [ "$test" = "wasi_components/io_redirect" ]; then
   # run test and capture its output
 
   # run the sys host test
-  cd instance/host_sys && cargo run > out.txt 2> err.txt && cd ../..
+  cd instance/host_sys && cargo test --lib -- --nocapture > out.txt 2> err.txt && cd ../..
   if [ $? -ne 0 ]; then
     cat *.txt
     echo
@@ -113,7 +113,7 @@ else
   # run test normally
 
   # run the sys host test
-  cd instance/host_sys && cargo run && cd ../..
+  cd instance/host_sys && cargo test --lib -- --nocapture && cd ../..
   if [ $? -ne 0 ]; then
     echo
     echo "Oh no, there is an error in the $test sys host."
