f2a95e6b3caaf598e0c072d9d2f3715440c95b00
kajacx <kajacx@gmail.com>
2024-01-14 12:33:46 +0100

    Remove old memory file

--- crates/wasm-bridge-js/src/direct/memory.rs
+++ crates/wasm-bridge-js/src/direct/memory.rs
@@ -1,6 +1,6 @@
 use std::{cell::RefCell, ops::Deref, rc::Rc};
 
-use anyhow::Context;
+use anyhow::{bail, Context};
 use js_sys::{Array, Function};
 use wasm_bindgen::JsValue;
 
@@ -9,60 +9,53 @@ use crate::{helpers::map_js_error, Result};
 use super::{Lower, ReadableMemory, WriteableMemory};
 
 #[derive(Debug, Clone)]
-pub struct ModuleMemoryInner {
-    memory: crate::Memory,
-    realloc: Function,
+pub(crate) struct ModuleMemory {
+    pub(crate) memory: crate::Memory,
+    pub(crate) realloc: Function,
 }
-
-#[derive(Debug, Clone, Default)]
-pub struct ModuleMemory(Rc<RefCell<Option<ModuleMemoryInner>>>);
 
 impl ModuleMemory {
-    pub fn new() -> Self {
-        Self(Rc::new(RefCell::new(Option::None)))
-    }
-
-    pub fn get(&self) -> impl Deref<Target = Option<ModuleMemoryInner>> + '_ {
-        self.0.borrow()
-    }
-
-    pub fn set(&self, memory: crate::Memory, realloc: Function) {
-        *self.0.borrow_mut() = Option::Some(ModuleMemoryInner::new(memory, realloc));
-    }
-}
-
-impl ModuleMemoryInner {
     pub(crate) fn new(memory: crate::Memory, realloc: Function) -> Self {
         Self { memory, realloc }
     }
 
     fn malloc(&self, align: usize, size: usize) -> Result<usize> {
-        let zero: JsValue = 0.into();
+        thread_local! {
+            static ARGS: [Array; 9] = [0,1,2,3,4,5,6,7,8].map(|align| {
+                let array = Array::new_with_length(4);
+                array.set(0, 0.into());
+                array.set(1, 0.into());
+                array.set(2, align.into());
+                array
+            });
+        }
 
-        // TODO: probably could re-use the same array
-        let args = Array::of4(&zero, &zero, &(align as u32).into(), &(size as u32).into());
-        let result = self
-            .realloc
-            .apply(&JsValue::UNDEFINED, &args)
-            .map_err(map_js_error("call capi_realloc"))?;
+        if align > 8 {
+            bail!("Align must be at most 8, it is {align} instead");
+        }
 
-        Ok(result.as_f64().context("realloc should return a number")? as usize)
+        ARGS.with(|align_args| {
+            let args = &align_args[align];
+            args.set(3, (size as u32).into());
+
+            let result = self
+                .realloc
+                .apply(&JsValue::UNDEFINED, args)
+                .map_err(map_js_error("call capi_realloc"))?;
+
+            Ok(result.as_f64().context("realloc should return a number")? as usize)
+        })
     }
 }
 
 impl WriteableMemory for ModuleMemory {
     fn allocate(&self, align: usize, size: usize) -> Result<ByteBuffer> {
-        let borrow = self.get();
-        let address = borrow.as_ref().unwrap().malloc(align, size)?;
+        let address = self.malloc(align, size)?;
         Ok(ByteBuffer::new(address, size))
     }
 
     fn flush(&self, slice: ByteBuffer) -> usize {
-        let borrow = self.get();
-        borrow
-            .as_ref()
-            .unwrap()
-            .memory
+        self.memory
             .write_impl(slice.address, &slice.data)
             .expect("write bytes to buffer"); // TODO: Can this fail? If so, why?
 
@@ -72,13 +65,51 @@ impl WriteableMemory for ModuleMemory {
 
 impl ReadableMemory for ModuleMemory {
     fn read_to_slice(&self, addr: usize, target: &mut [u8]) {
-        let borrow = self.get();
-        borrow
-            .as_ref()
-            .unwrap()
-            .memory
+        self.memory
             .read_impl(addr, target)
-            .expect("Read memory should work")
+            .expect("read bytes from memory")
+    }
+}
+
+#[derive(Debug, Clone, Default)]
+pub(crate) struct LazyModuleMemory(Rc<RefCell<Option<ModuleMemory>>>);
+
+impl LazyModuleMemory {
+    pub(crate) fn new() -> Self {
+        Self(Rc::new(RefCell::new(Option::None)))
+    }
+
+    pub(crate) fn get(&self) -> impl Deref<Target = Option<ModuleMemory>> + '_ {
+        self.0.borrow()
+    }
+
+    pub(crate) fn set(&self, module_memory: ModuleMemory) {
+        *self.0.borrow_mut() = Some(module_memory);
+    }
+}
+
+impl WriteableMemory for LazyModuleMemory {
+    fn allocate(&self, align: usize, size: usize) -> Result<ByteBuffer> {
+        self.get()
+            .as_ref()
+            .expect("initialized lazy memory")
+            .allocate(align, size)
+    }
+
+    fn flush(&self, buffer: ByteBuffer) -> usize {
+        self.get()
+            .as_ref()
+            .expect("initialized lazy memory")
+            .flush(buffer)
+    }
+}
+
+impl ReadableMemory for LazyModuleMemory {
+    fn read_to_slice(&self, addr: usize, target: &mut [u8]) {
+        self.get()
+            .as_ref()
+            .expect("initialized lazy memory")
+            .read_to_slice(addr, target)
     }
 }
 
@@ -100,7 +131,6 @@ impl ByteBuffer {
     }
 
     pub fn skip(&mut self, num_bytes: usize) {
-        // TODO: could just skip instead of writing 0s with some unsafe magic
         self.data.extend((0..num_bytes).map(|_| 0));
     }
 
