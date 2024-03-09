d6dbbca831fec3a5d7b2d031b436ad45499b1987
kajacx <kajacx@gmail.com>
2024-03-09 17:55:59 +0100

    Fix forgotten comment in test script

--- tests/skeletons/wit_components/run_test.sh
+++ tests/skeletons/wit_components/run_test.sh
@@ -19,29 +19,29 @@ cp $test/host.rs instance/host_sys/src/host.rs
 cp $test/host.rs instance/host_js/src/host.rs
 cp $test/host.rs instance/host_js_opt/src/host.rs
 
-# # run the sys host test
-# cd instance/host_sys && cargo test --lib -- --nocapture && cd ../..
-# if [ $? -ne 0 ]; then
-#   echo
-#   echo "Oh no, there is an error in the $test sys host."
-#   echo "Inspect the instance folder for more detail."
-#   exit 1
-# fi
+# run the sys host test
+cd instance/host_sys && cargo test --lib -- --nocapture && cd ../..
+if [ $? -ne 0 ]; then
+  echo
+  echo "Oh no, there is an error in the $test sys host."
+  echo "Inspect the instance folder for more detail."
+  exit 1
+fi
 
-# # run the js host test
-# cd instance/host_js && wasm-pack test --node && cd ../..
-# if [ $? -ne 0 ]; then
-#   echo
-#   echo "Oh no, there is an error in the $test js host."
-#   echo "Inspect the instance folder for more detail."
-#   exit 1
-# fi
-
-# run the js host test with new code
-cd instance/host_js_opt && wasm-pack test --node && cd ../..
+# run the js host test
+cd instance/host_js && wasm-pack test --node && cd ../..
 if [ $? -ne 0 ]; then
   echo
   echo "Oh no, there is an error in the $test js host."
   echo "Inspect the instance folder for more detail."
   exit 1
 fi
+
+# run the js host test with new code
+# cd instance/host_js_opt && wasm-pack test --node && cd ../..
+# if [ $? -ne 0 ]; then
+  # echo
+  # echo "Oh no, there is an error in the $test js optimized host."
+  # echo "Inspect the instance folder for more detail."
+  # exit 1
+# fi
