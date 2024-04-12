eb56ca48e7696e8ba700279df29836a6de90a2fa
kajacx <kajacx@gmail.com>
2024-04-12 21:13:29 +0200

    Update interfaces guest

--- tests/wit_components/interfaces/guest.rs
+++ tests/wit_components/interfaces/guest.rs
@@ -11,9 +11,7 @@ impl Guest for GuestImpl {
     }
 }
 
-struct GuestImplAdd;
-
-impl exports::component_test::wit_protocol::guest_add::Guest for GuestImplAdd {
+impl exports::component_test::wit_protocol::guest_add::Guest for GuestImpl {
     fn add_three(num: i32) -> i32 {
         let num = component_test::wit_protocol::host_add::add_one(num);
         let num = component_test::wit_protocol::host_add::add_one(num);
@@ -22,9 +20,7 @@ impl exports::component_test::wit_protocol::guest_add::Guest for GuestImplAdd {
     }
 }
 
-struct GuestImplSub;
-
-impl exports::guest_sub::Guest for GuestImplSub {
+impl exports::guest_sub::Guest for GuestImpl {
     fn sub_three(num: i32) -> i32 {
         let num = host_sub::sub_one(num);
         let num = host_sub::sub_one(num);
